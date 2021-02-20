7b3dc46c5e4b65df03acc27387094e3ce26cefaa
Shoury Sharma <shanuu12e@gmail.com>
2021-02-20 19:07:29 +0530

    Made little changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/home',
+    initialRoute: '/login',
     routes: {
       '/login':(context) => Login(),
       '/home':(context) => Home(),
